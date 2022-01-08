b284fd4d0c3b0f8e8042780a83c1c77d9f226c6c
Soto Conuecar,Miguel M <m.sotoconuecar@student.fontys.nl>
2022-01-08 14:40:12 +0100

    added k6 script for load testing

---
+++ k6 scripts/script.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+
+export default function () {
+  const url = 'http://20.103.122.194/api/Authentication';
+  const payload = JSON.stringify({
+  userId: 0,
+  userName: "miguelsoto",
+  email: "string",
+  password: "johnnyiscool",
+  admin: true
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  http.post(url, payload, params);
+}
